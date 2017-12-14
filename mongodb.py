baa4ef640903662b2f7644e23e06aad6759df437
rneman <34446538+rneman@users.noreply.github.com>
2017-12-15 00:00:46 +0200

    Delete mongodb.py

--- mongodb.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-# heroku_32c6n0hx <-- database name.
-
-client = MongoClient(mongodb://heroku_32c6n0hx:hf9dugec2vd58c6maoi7orpa2u@ds139446.mlab.com:39446/heroku_32c6n0hx)
-
-db = client.heroku_32c6n0hx
