e84bdc4d71ea36718f58bca4a8a39dff996582be
Rasmus <rasmus.lindman@protonmail.com>
2017-12-14 23:52:42 +0200

    MongoDB first try.

---
+++ mongodb.py
@@ -0,0 +1,7 @@
+from pymongo import MongoClient
+
+# heroku_32c6n0hx <-- database name.
+
+client = MongoClient(mongodb://heroku_32c6n0hx:hf9dugec2vd58c6maoi7orpa2u@ds139446.mlab.com:39446/heroku_32c6n0hx)
+
+db = client.heroku_32c6n0hx
